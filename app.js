12646a5f06dcadd3bf126f8b1f77d972afe828c7
Steven Moyano <stevenmoyano1@gmail.com>
2022-05-16 14:29:32 -0500

    completed additional tasks

--- app.js
+++ app.js
@@ -1,9 +1,17 @@
 const express = require("express");
 const app = express();
-const port = 3000;
+const port = 3001;
 
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+  res.status(200).send("Stevens");
+});
+
+app.get("/order", (req, res) => {
+  res.status(201).send("Welcome to the order page!");
+});
+
+app.get("*", (req, res) => {
+  res.status(404).send("Page not found");
 });
 
 app.listen(port, () => {
